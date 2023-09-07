6d939d3676d6f007a36b6f79b7aaa14f90731493
Florescu Cristian <qflorescucristian@gmail.com>
2023-09-07 11:32:18 +0300

    Added singleton pattern example.

---
+++ car-service-singleton-pattern/CarService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car_service_singleton_pattern
+{
+    public class CarService
+    {
+        private static readonly object _lock = new object();
+        private static CarService? _carService = null;
+        private List<String> _cars;
+
+        private CarService()
+        {
+            _cars = new List<String>();
+        }
+
+        public static CarService GetCarService()
+        {
+            lock (_lock)
+            {
+                if(_carService == null)
+                {
+                    _carService = new CarService();
+                }
+                return _carService;
+            }
+        }
+
+        public List<String> Cars
+        {
+            get { return _cars; }
+            set
+            {
+                _cars = value;
+            }
+        }
+    }
+}
---
+++ car-service-singleton-pattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using car_service_singleton_pattern;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CarService carService1 = CarService.GetCarService();
+        CarService carService2 = CarService.GetCarService();
+        carService1.Cars.Add("Dacia");
+        Console.WriteLine(carService2.Cars[0]);
+    }
+}
